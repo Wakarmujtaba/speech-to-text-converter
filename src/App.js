bba8d3f514346b685baeeda3c4719210aa37ba6c
Wakar Mujtaba <wakarmujtaba2@gmail,com>
2023-11-03 22:02:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useState } from 'react';
+import useClipboard from "react-use-clipboard";
+
+
+
+
+function App() {
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+      successDuration:1000
+  });
+  const startlistening= ()=>SpeechRecognition.startListening({ continuous: true,language:'en-IN' });
+  const stoplstening = ()=>SpeechRecognition.stopListening();
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    // return 'sahi se bol bhai';
+  }
+  return (
+    <div className="container">
+      <h2>Convert  Speech to Text</h2>
+      <label> <a href="https://www.linkedin.com/in/wakar-mujtaba-457520190/"> By Wakar Mujtaba</a> </label>
+      <br />
+
+      <p >  </p>
+       <button onClick={refreshpage} style={{background:'maroon', marginTop:'1px'}}>Reset</button>
+      <div className='main-content' onClick={() =>  setTextToCopy(transcript)}>
+      {transcript}
+      </div>
+
+      <div className='btn-style' >
+      <button onClick={setCopied} style={{background:'chartreuse'}}>
+       {isCopied ? 'Copied!' : 'Copy'}
+       
+      </button>
+
+        <button onClick={startlistening}>Start</button>
+        <button onClick={stoplstening} style={{background:'red'}}>stop</button>
+      </div>
+    </div>
+  );
+}
+
+function refreshpage(){
+window.location.reload(false);
+}
+export default App;
